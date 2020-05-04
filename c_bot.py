aa101b4c7e19399f6652cdd79b34f66b2847ecfc
Gobral <umpalump97@gmail.com>
2020-05-04 03:52:00 +0200

    befor minmax implementation

--- c_bot.py
+++ c_bot.py
@@ -1,43 +1,80 @@
 import numpy as np
 from c_node import C_Node
+import time
 
 class C_Bot:
     def __init__(self, plansza, kolor, glebokosc):
+        self.zt_aktualny_czas = time.time()
         self.znp_plansza = plansza
         self.zi_kolor = kolor
         self.zi_glebokosc = glebokosc
+        self.win_state = 4
+
+    def f_wyswietl_pomiar(self, label):
+        zt_nowy = time.time()
+        print(label, "   ", zt_nowy - self.zt_aktualny_czas)
+        self.zt_aktualny_czas = zt_nowy
 
     def f_wykonaj_ruch(self):
+        self.f_wyswietl_pomiar("Start sprawdzaia")
         if np.sum(self.znp_plansza) == 0:
             self.znp_plansza[5][3] = self.zi_kolor
             return
         
         root = C_Node(self.znp_plansza)
-        aktualny = root
         akt_kolor = self.zi_kolor
-        ruchy = self.f_generuj_ruchy(aktualny.znp_stan, akt_kolor)
+        ruchy = self.f_generuj_ruchy(root.znp_stan, akt_kolor)
         liscie = []
+        analiza = []
         for ruch in ruchy:
-            l = aktualny.f_dodaj_dziecko()
+            l = root.f_dodaj_dziecko()
             l.color = akt_kolor
             l.znp_stan[ruch[0]][ruch[1]] = ruch[2]
-            self.f_ocen_node(l)
-            print(l.znp_stan)
-            print(l.score)
+            self.f_ocen_node(l, l.color)
 
             liscie.append(l)
 
 
         licznik = 1
         while licznik <= self.zi_glebokosc:
+            if licznik == self.zi_glebokosc:
+                analiza = liscie[:]
+            print(len(liscie))
             if akt_kolor == 1:
                 akt_kolor = 2
             else:
                 akt_kolor = 1
             nowe_liscie = []
-            #for l in liscie:
+            for l in liscie:
+                ruchy = self.f_generuj_ruchy(l.znp_stan, akt_kolor)
+                for ruch in ruchy:
+                    nl = l.f_dodaj_dziecko()
+                    nl.color = akt_kolor
+                    nl.znp_stan[ruch[0]][ruch[1]] = ruch[2]
+
+                    self.f_ocen_node(nl, nl.color)
+                    if nl.score < self.win_state:
+                        nowe_liscie.append(nl)
+                    
+                    if akt_kolor != self.zi_kolor:
+                        self.f_ocen_node(nl, self.zi_kolor)
+
+            liscie = nowe_liscie
 
             licznik += 1 
+
+        print(len(liscie))
+        print(len(analiza))
+
+        #minmax
+        while(analiza[0] != None):
+            analiza = self.f_generuj_poziom(root)
+        
+        
+        self.f_wyswietl_pomiar("Konic")
+
+    def f_generuj_poziom(self, root):
+        return [None]
 
     def f_generuj_ruchy(self, stan, kolor):
         ret_ruchy = []
@@ -51,8 +88,10 @@ class C_Bot:
         
         return ret_ruchy
 
-    def f_ocen_node(self, node):
+    def f_ocen_node(self, node, kolor):
         ocena = 0
+        temp_kolor = node.color
+        node.color = kolor
         for kol in node.znp_stan.T:
             i = len(kol) -1
             licznik = 0
@@ -60,6 +99,8 @@ class C_Bot:
                 if kol[i] == node.color:
                     licznik += 1
                 elif kol[i] != 0:
+                    if licznik >= self.win_state:
+                        ocena = licznik
                     licznik = 0
                 else:
                     break
@@ -67,8 +108,144 @@ class C_Bot:
             if licznik > ocena:
                 ocena = licznik
 
+        for wier in node.znp_stan:
+            if np.sum(wier) != 0:
+                licznik = 0
+                zera = 0
+                for k in wier:
+                    if k == node.color:
+                        licznik += 1
+                    elif k != 0:
+                        if zera > 0 and licznik > 0 and licznik > ocena:
+                            ocena = licznik
+                        if licznik >= self.win_state:
+                            ocena = licznik
+                        licznik = 0
+                        zera = 0
+                    else:
+                        if licznik > ocena:
+                            ocena = licznik
+                        licznik = 0
+                        zera += 1
+                if licznik > ocena:
+                    ocena = licznik
+
+        # przeszukiwanie od dołu na ukos w prawo
         for kol in range(0, len(node.znp_stan[0]) - 3):
-            i = len(node.znp_stan)
+            i = len(node.znp_stan) - 1
+            j = kol 
+            zera = 0
+            licznik = 0
+            while j < len(node.znp_stan[0]) and i >= 0:
+                if node.znp_stan[i][j] == node.color:
+                    licznik += 1
+                elif node.znp_stan[i][j] != 0:
+                    if zera > 0 and licznik > 0 and licznik > ocena:
+                        ocena = licznik
+                    if licznik >= self.win_state:
+                        ocena = licznik
+                    licznik = 0
+                    zera = 0
+                else:
+                    if licznik > ocena:
+                        ocena = licznik
+                    licznik = 0
+                    zera += 1
+        
+                j += 1
+                i -= 1
+
+            if licznik > ocena:
+                ocena = licznik
+        
+        # przeszukiwanie od dołu na ukos w lewo
+        for kol in range(3, len(node.znp_stan[0])):
+            i = len(node.znp_stan) - 1
+            j = kol 
+            
+            zera = 0
+            licznik = 0
+            while j >= 0 and i >= 0:
+                if node.znp_stan[i][j] == node.color:
+                    licznik += 1
+                elif node.znp_stan[i][j] != 0:
+                    if zera > 0 and licznik > 0 and licznik > ocena:
+                        ocena = licznik
+                    if licznik >= self.win_state:
+                        ocena = licznik
+                    licznik = 0
+                    zera = 0
+                else:
+                    if licznik > ocena:
+                        ocena = licznik
+                    licznik = 0
+                    zera += 1
+
+                j -= 1
+                i -= 1
+
+            if licznik > ocena:
+                ocena = licznik
+
+        # przeszukiwanie na ukos w prawo pozostale
+        for wier in range(len(node.znp_stan) - 3, len(node.znp_stan) - 1):
+            if wier > 0:
+                i = wier
+                j = 0
+                zera = 0
+                licznik = 0
+                while j < len(node.znp_stan[0]) and i >= 0:
+                    if node.znp_stan[i][j] == node.color:
+                        licznik += 1
+                    elif node.znp_stan[i][j] != 0:
+                        if zera > 0 and licznik > 0 and licznik > ocena:
+                            ocena = licznik
+                        if licznik >= self.win_state:
+                            ocena = licznik
+                        licznik = 0
+                        zera = 0
+                    else:
+                        if licznik > ocena:
+                            ocena = licznik
+                        licznik = 0
+                        zera += 1
+
+                    j += 1
+                    i -= 1
+
+                if licznik > ocena:
+                    ocena = licznik
+        
+        # przeszukiwanie na ukos w lewo pozostale
+        for wier in range(len(node.znp_stan) - 3, len(node.znp_stan) - 1):
+            if wier > 0:
+                i = wier
+                j = len(node.znp_stan[0]) - 1
+                zera = 0
+                licznik = 0
+                while j >= 0 and i >= 0:
+                    if node.znp_stan[i][j] == node.color:
+                        licznik += 1
+                    elif node.znp_stan[i][j] != 0:
+                        if zera > 0 and licznik > 0 and licznik > ocena:
+                            ocena = licznik
+                        if licznik >= self.win_state:
+                            ocena = licznik
+                        licznik = 0
+                        zera = 0
+                    else:
+                        if licznik > ocena:
+                            ocena = licznik
+                        zera += 1
+                        licznik = 0
+
+                    j -= 1
+                    i -= 1
+
+                if licznik > ocena:
+                    ocena = licznik
+        
         node.score = ocena
+        node.color = temp_kolor
 
         
--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from c_bot import C_Bot
 import numpy as np
 
 znp_plansza = np.zeros((6,7), np.int8)
+znp_plansza[5][4] = 1
+znp_plansza[5][5] = 2
+znp_plansza[5][6] = 1
+znp_plansza[4][5] = 1
+znp_plansza[4][4] = 2
+znp_plansza[3][4] = 1
+znp_plansza[5][3] = 2
+znp_plansza[4][3] = 2
+
+
 zb_bot = C_Bot(znp_plansza, 1, 4)
-zb_bot.f_wykonaj_ruch()
 zb_bot.f_wykonaj_ruch()
 print(znp_plansza)
